2b4c6f0b264d3bb9aab1ac39e4184bada4fe286b
Fteeliy <Fteeliy@yandex.ru>
2021-03-19 20:16:41 +0300

    create: mobile mebu

--- js/main.js
+++ js/main.js
@@ -43,3 +43,8 @@ var reviewsSlider = new Swiper('.reviews-slider', {
             zoom: 15
         });
     }
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке Меню");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
