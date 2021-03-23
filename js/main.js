cceb9fee3447322fc12e6f08f549dc01aab6c8c8
Fteeliy <Fteeliy@yandex.ru>
2021-03-23 17:20:31 +0300

    create & fixed: modal form, validation

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
 $(document).ready(function () {
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Keyboard control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow",
-});
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // Keyboard control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
-// Функция ymaps.ready() будет вызвана, когда
-    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [7.838196, 98.298813],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 15
-        });
-    }
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Keyboard control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: "coverflow",
+  });
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Keyboard control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [7.838196, 98.298813],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 15
+    });
+  }
   var menuButton = $('.menu-button');
   menuButton.on("click", function () {
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
@@ -52,23 +52,42 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal)
-  closeModalButton.on('keydown', closeModal)
+  closeModalButton.on('click', closeModal);
+  $(document).keyup(closeModal);
+  $(".modal__overlay").on("click", closeModal);
 
   function openModal() {
     var targetModal = $(this).attr("data-href");
     $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
     $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
-
+    $('body').css('overflow','hidden');
   }
   function closeModal(event) {
+    $('body').css('overflow','auto');
+    if (event.key === "Escape" || event.type === 'click') {
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
     event.preventDefault();
-    var modalOverlay = $('.modal__overlay');
-    var modalDialog = $('.modal__dialog');
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
-    if (event.keyCode == 27)
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
+    
   }
+  // Обработка форм
+  $('.form').each(function() {
+    $(this).validate({messages: {
+    errorClass: "invalid",
+    name: {
+      required: "Укажите имя",
+      minlength: "Имя должно быть не короче двух букв",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+    phone: {
+      required: "Телефон обязателен",
+    },
+  }});
+  });
 });
